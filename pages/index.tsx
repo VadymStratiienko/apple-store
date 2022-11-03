c73fd7f8e748ce730de1b16b92755bf7c35ac46f
VadymStratiienko <vadymstratiienko44@gmail.com>
2022-11-03 11:38:40 +0200

    start

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Lest build Apple Store</h1>
+
+    </div>
+  )
+}
+
+export default Home
+
