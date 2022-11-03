97a658aa0e119dd14be7741ed42084b4fa55a668
VadymStratiienko <vadymstratiienko44@gmail.com>
2022-11-03 16:44:12 +0200

    add Header and Lading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
+import Landing from '../components/Landing'
 
 const Home: NextPage = () => {
   return (
     <div className="">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Apple Store</title>
+        <link rel="icon" href="http://rb.gy/vsvv2o" />
       </Head>
-      <h1>Lest build Apple Store</h1>
-
+      <Header />
+        <main>
+          <Landing />
+        </main>
     </div>
   )
 }
